0bb2c0153c236c11d82c23414e89aefe3fbdd1fa
HuynhNgocThuong <huynhngocthuong0208@gmail>
2021-11-08 22:32:38 +0700

    [Add]: Leetcode 2020/11/08

---
+++ js/PlusOne.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+  console.log(digits.join(""));
+  sumDigits = BigInt(digits.join("")) + 1n;
+  console.log(sumDigits);
+  strSumDigits = sumDigits.toString();
+  result = strSumDigits.split("");
+  return result;
+};
+
+console.log(plusOne());
